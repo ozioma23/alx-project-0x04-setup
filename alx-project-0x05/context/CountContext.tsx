f45f6d4b407dedd2791f1b497b784244d98525d6
OgborFaith <faithogbor23@gmail.com>
2025-11-25 22:35:38 +0100

    corrections made to the CountContext.tsx file

--- alx-project-0x05/context/CountContext.tsx
+++ alx-project-0x05/context/CountContext.tsx
@@ -2,8 +2,8 @@ mport { createContext, useContext,  useState, ReactNode } from "react"
 
 interface CountContextProps {
   count: number
-  increment: () => void
-  decrement: () => void
+  increment: () => void;
+  decrement: () => void;
 }
 
 export const CountContext = createContext<CountContextProps | undefined>(undefined)
